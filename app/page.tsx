36cc01a5f550b407afd1c9b608009bf048a40fd3
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2025-04-21 17:24:54 +0300

    fixed the app page

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ async function BlogPosts() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       {data.map((item, index) => (
-        <BlogCard key={index} item={item} index={index} />
+        <BlogCard key={index} item={item} />
       ))}
     </div>
   );
